5636cbd25a811a23f053d07054a4034f1ecf5ed6
lekmiti <lekmiti1992@gmail.com>
2019-03-10 11:36:24 +0100

    #3 add reactive rest-api layer

---
+++ src/main/java/com/lekmiti/reativewebfluxlab/BookReactiveResource.java
@@ -0,0 +1,9 @@
+package com.lekmiti.reativewebfluxlab;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookReactiveResource {
+}
