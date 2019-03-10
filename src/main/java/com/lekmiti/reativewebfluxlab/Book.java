959ce211109c8f55db925f575fc22c732224319f
lekmiti <lekmiti1992@gmail.com>
2019-03-10 11:32:58 +0100

    #3 add model layer

---
+++ src/main/java/com/lekmiti/reativewebfluxlab/Book.java
@@ -0,0 +1,18 @@
+package com.lekmiti.reativewebfluxlab;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@Entity
+public class Book {
+    @Id
+    private String title;
+    private String author;
+    private int year;
+    private float cost;
+}
