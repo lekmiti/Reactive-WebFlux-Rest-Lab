ad168feea70e75bde95707095cbe8e218e19518a
lekmiti <lekmiti1992@gmail.com>
2019-03-10 10:50:53 +0100

    #1 init project

---
+++ src/main/java/com/lekmiti/reativewebfluxlab/ReativeWebfluxLabApplication.java
@@ -0,0 +1,13 @@
+package com.lekmiti.reativewebfluxlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReativeWebfluxLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReativeWebfluxLabApplication.class, args);
+	}
+
+}
